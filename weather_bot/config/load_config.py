6bd322814f5cd82891662acde3da119baa1a33a2
https://github.com/Merhemet-Huseynov <merhemethuseynov795@gmail.com>
2025-01-06 17:16:40 +0400

    perf: created a load_config function to set dynamic file paths

--- weather_bot/config/load_config.py
+++ weather_bot/config/load_config.py
@@ -1,14 +1,22 @@
+from pathlib import Path
+
 def load_config():
     config = {}
-    with open("D:/weather_bot/Weather-Bot-Azerbaijan/weather_bot/config/config.txt", "r") as file:
+    
+    # Fayl yolunu düzgün qur
+    config_path = Path(__file__).resolve().parent / "config.txt"
+    
+    # Faylı aç və oxu
+    with open(config_path, "r") as file:
         for line in file:
-            # Hər bir xətti oxuyub "=" işarəsindən bölüb dəyəri təyin edirik
+            # "key=value" formatında məlumatları parçala
             if "=" in line:
-                key, value = line.strip().split("=")
+                key, value = line.strip().split("=", 1)
                 config[key] = value
+
     return config
 
+# Konfiqurasiya yüklə
 config = load_config()
 BOT_TOKEN = config.get("BOT_TOKEN")
 WEATHER_API_KEY = config.get("WEATHER_API_KEY")
-
